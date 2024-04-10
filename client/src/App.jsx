02ef66ab085872f13f68ca6101f6e07fe84d6a5f
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-04-09 21:39:11 -0300

    Rotas criadas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,33 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 
 const App = () => {
   
-  return <Home/>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path= '/' element={<Home/>}>
+        </Route>
+        <Route path= '/products/:category' element={<ProductList/>}>
+        </Route>
+        <Route path= '/product/:id' element={<Product/>}>
+        </Route>
+        <Route path= '/cart' element={<Cart/>}>
+        </Route>
+        <Route path= '/login' element={<Login/>}>
+        </Route>
+        <Route path= '/register' element={<Register/>}>
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
