9efccce991a4a081e930f83f69f225640bead88f
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-12-21 22:17:00 -0300

    feature - product creation in admin panel added

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEe7U2lcNww9nVCAKrybRcCGg4OfKtvkI",
+  authDomain: "shop-d701c.firebaseapp.com",
+  projectId: "shop-d701c",
+  storageBucket: "shop-d701c.firebasestorage.app",
+  messagingSenderId: "121619736977",
+  appId: "1:121619736977:web:2519ac3e300fb553d89230"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,30 +1,65 @@
+import { useState } from "react";
 import "./newProduct.css";
+import {
+  getStorage,
+  ref
+} from "firebase/storage";
+import app from "../../firebase"
+
 
 export default function NewProduct() {
+  const [inputs, setInputs] = useState({});
+  const [file, setFile] = useState(null);
+  const [cat, setCat] = useState([]);
+
+  const handleChange = (e) => { 
+    setInputs((prev) => {
+      return {...prev, [e.target.name]:e.target.value}
+    })
+  }
+  const handleCat = (e) => { 
+    setCat(e.target.value.split(","));
+  }
+  const handleClick = (e) => {
+    e.preventDefault();
+    const fileName = new Date().getTime() + file.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName)
+  }
+  console.log(file)
+
   return (
     <div className="newProduct">
-      <h1 className="addProductTitle">New Product</h1>
+      <h1 className="addProductTitle">Novo Produto</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input type="file" id="file" onChange={e => setFile(e.target.files[0])}/>
         </div>
         <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <label>Title</label>
+          <input name="title" type="text" placeholder="Apple Airpods" onChange={handleChange}/>
         </div>
         <div className="addProductItem">
-          <label>Stock</label>
-          <input type="text" placeholder="123" />
+          <label>Description</label>
+          <input name="desc" type="text" placeholder="Description ..." onChange={handleChange}/>
         </div>
         <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
+          <label>Price</label>
+          <input name="price" type="number" placeholder="R$ 100"  onChange={handleChange}/>
+        </div>
+        <div className="addProductItem">
+          <label>Categories</label>
+          <input name="cat" type="text" placeholder="Fitas, Disjuntores, Chuveiros" onChange={handleCat}/>
+        </div>
+        <div className="addProductItem">
+          <label>Em estoque</label>
+          <select name="inStock" id="" onChange={handleChange}>
+            <option value="true">Sim</option>
+            <option value="false">Não</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+        <button onClick={handleClick} className="addProductButton">Create</button>
       </form>
     </div>
   );
